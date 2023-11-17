094f0e5fd54140831d646c538f05b172730c584e
huysonny <huy26072003h@gmail.com>
2023-11-17 23:40:48 +0700

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,69 @@
+const products = [
+  {
+    name: "Premium Prada Fashion Men's Sheepskin Coat and Pants Set, Stylish and Trendy",
+    image: "/images/anh1.jpg",
+    description: `Elevate your style with this high-end Prada fashion set for men, featuring a luxurious sheepskin coat and matching pants. The exquisite craftsmanship and attention to detail make this ensemble a statement of sophistication and individuality. The premium materials used in the construction ensure both comfort and durability.
+
+      This set is designed for the modern, fashion-forward man who appreciates quality and distinctive style. The Prada logo subtly enhances the overall appeal, reflecting the brand's commitment to luxury.`,
+    brand: "Prada",
+    category: "jacket",
+    price: 189.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: " High-End DIOR Men's Winter Fleece Clothing Set, Youthful and Dynamic.",
+    image: "/images/anh2.jpg",
+    description: `Embrace the winter season in style with this premium DIOR fleece clothing set for men. Designed with a youthful and dynamic flair, this ensemble seamlessly blends comfort and fashion. The high-quality fleece material ensures warmth and coziness during chilly days, making it an ideal choice for the discerning gentleman.
+
+      The DIOR logo subtly enhances the overall aesthetic, showcasing the brand's dedication to both luxury and contemporary trends. Whether you're heading out for a casual day or a social gathering, this winter clothing set from DIOR will undoubtedly make a fashion statement.`,
+    brand: "DiOR",
+    category: "sweater",
+    price: 141.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "High-End DIOR Long-Sleeve Patterned Shirt, Fashionably Designed and Unique",
+    image: "/images/anh3.jpg",
+    description: `Add a touch of luxury and personality to your wardrobe with this premium DIOR long-sleeve patterned shirt. The fashion-forward design and high-quality craftsmanship make this shirt a standout piece in men's fashion. The intricate patterns showcase DIOR's commitment to blending sophistication with contemporary style.
+
+      Crafted for the modern man who appreciates distinctive fashion, this DIOR shirt is perfect for both casual and semi-formal occasions. The attention to detail and the iconic DIOR logo enhance the overall appeal, making it a must-have for those who seek a unique and stylish look.`,
+    brand: "DIOR",
+    category: "Shirts",
+    price: 71.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Premium Jungle Buffalo Leather Men's Jacket - JK01, Exquisitely Crafted for Tropical Southeast Asia",
+    image: "/images/anh4.jpg",
+    description: `Introducing the JK01, a luxurious buffalo leather jacket tailored for the discerning man in tropical Southeast Asia. This high-end piece is crafted from premium jungle buffalo leather, known for its durability and rugged aesthetic. The meticulous design reflects both sophistication and functionality, making it an ideal choice for those seeking an elevated outerwear experience.
+
+      The JK01 jacket is a symbol of style and resilience, perfect for navigating the dynamic climates of the region. The rich texture of the jungle buffalo leather adds a unique character to the jacket, showcasing its exclusivity. The attention to detail and superior craftsmanship ensure a comfortable and stylish fit, making it a versatile addition to your wardrobe.`,
+    brand: "Sony",
+    category: `
+    leather jacket`,
+    price: 259.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "SuzaSport Premium Double-Layer Men's Khaki Jacket - Code AK03. Fashionable Urban Style for the Modern Man",
+    image: "/images/anh5.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Jacket",
+    category: "Superme",
+    price: 29.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+];
+
+export default products;
